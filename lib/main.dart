ba9cf323850c32df4022a7b5d44fa03c50674040
sreekanthsanthosh17 <sreekanthsanthosh17@gmail.com>
2024-12-02 14:50:42 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CalculatorApp());
+}
+
+class CalculatorApp extends StatelessWidget {
+  const CalculatorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const CalculatorHome(),
+    );
+  }
+}
+
+class CalculatorHome extends StatefulWidget {
+  const CalculatorHome({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _CalculatorHomeState createState() => _CalculatorHomeState();
+}
+
+class _CalculatorHomeState extends State<CalculatorHome> {
+  String _output = "0";
+  String _result = "0";
+  String _operator = "";
+  double _num1 = 0;
+  double _num2 = 0;
+
+  void _buttonPressed(String buttonText) {
+    setState(() {
+      if (buttonText == "C") {
+        _output = "0";
+        _result = "0";
+        _operator = "";
+        _num1 = 0;
+        _num2 = 0;
+      } else if (buttonText == "+" ||
+          buttonText == "-" ||
+          buttonText == "x" ||
+          buttonText == "/") {
+        _num1 = double.parse(_output);
+        _operator = buttonText;
+        _output = "0";
+      } else if (buttonText == "=") {
+        _num2 = double.parse(_output);
+        switch (_operator) {
+          case "+":
+            _result = (_num1 + _num2).toString();
+            break;
+          case "-":
+            _result = (_num1 - _num2).toString();
+            break;
+          case "x":
+            _result = (_num1 * _num2).toString();
+            break;
+          case "/":
+            _result = (_num1 / _num2).toString();
+            break;
+        }
+        _output = _result;
+        _operator = "";
+      } else {
+        if (_output == "0") {
+          _output = buttonText;
+        } else {
+          _output += buttonText;
+        }
+      }
+    });
+  }
+
+  Widget _buildButton(String buttonText) {
+    return Expanded(
+      child: OutlinedButton(
+        onPressed: () => _buttonPressed(buttonText),
+        child: Text(
+          buttonText,
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calculator'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            alignment: Alignment.centerRight,
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 20),
+            child: Text(
+              _output,
+              style: const TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+            ),
+          ),
+          const Expanded(
+            child: Divider(),
+          ),
+          Column(
+            children: [
+              Row(
+                children: [
+                  _buildButton("7"),
+                  _buildButton("8"),
+                  _buildButton("9"),
+                  _buildButton("/"),
+                ],
+              ),
+              Row(
+                children: [
+                  _buildButton("4"),
+                  _buildButton("5"),
+                  _buildButton("6"),
+                  _buildButton("x"),
+                ],
+              ),
+              Row(
+                children: [
+                  _buildButton("1"),
+                  _buildButton("2"),
+                  _buildButton("3"),
+                  _buildButton("-"),
+                ],
+              ),
+              Row(
+                children: [
+                  _buildButton("C"),
+                  _buildButton("0"),
+                  _buildButton("="),
+                  _buildButton("+"),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
